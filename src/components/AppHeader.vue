ffc3afa76a8d1d70ba3cdd36917fee1911429da2
Justin Ho Tuan Duong <zamakkat@gmail.com>
2018-10-01 15:48:33 +0800

    Add more content to pages

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,15 @@
 <template>
-  <header>
+  <header class='container'>
     <nav>
-      <ul>
-        <li>
-          <router-link to='/'>Home</router-link>
+      <ul class='nav-menu'>
+        <li class='nav-item'>
+          <router-link to='/' class='nav-link'>Home</router-link>
         </li>
-        <li>
-          <router-link to='/about'>About Us</router-link>
+        <li class='nav-item'>
+          <router-link to='/about' class='nav-link'>About Us</router-link>
         </li>
-        <li>
-          <router-link to='/contact'>Contact Us</router-link>
+        <li class='nav-item'>
+          <router-link to='/contact' class='nav-link'>Contact Us</router-link>
         </li>
       </ul>
     </nav>
@@ -23,5 +23,31 @@ export default {
 </script>
 
 <style scoped>
+  .nav-menu {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    margin: 0;
+  }
 
+  .nav-item {
+    list-style: none;
+    margin-left: 10px;
+  }
+
+  .nav-item:first-child {
+    margin-left: 0;
+  }
+
+  .nav-link {
+    background-color: #42b983;
+    border-radius: 5px;
+    color: #fff;
+    padding: 10px 20px;
+    text-decoration: none;
+  }
+
+  .nav-link:hover {
+    background-color: #3ba675;
+  }
 </style>
--- src/components/HomeContact.vue
+++ src/components/HomeContact.vue
@@ -1,6 +1,22 @@
 <template>
   <div class='home-contact'>
     <h1>Contact Us</h1>
+    <p>
+       Suspendisse potenti. Donec lacus odio, scelerisque quis rutrum vitae, aliquet vel eros. Maecenas sagittis posuere hendrerit. In pulvinar ullamcorper nisi aliquet facilisis. Donec magna justo, egestas nec vulputate at, gravida eget enim. Aliquam erat volutpat. Integer ullamcorper tincidunt rhoncus. Donec cursus nisl accumsan convallis pellentesque. Ut lectus lacus, faucibus vitae massa eu, sagittis commodo augue. Vivamus fringilla diam et ante condimentum tincidunt. Nam fringilla tellus vel turpis malesuada, maximus viverra quam porttitor. Maecenas at feugiat sem. Pellentesque lacinia orci convallis eros auctor posuere non ac elit.
+    </p>
+    <figure>
+      <img src='https://source.unsplash.com/daily?sand' alt=''>
+      <figcaption>Photo on <a href='https://unsplash.com/'>Unsplash</a></figcaption>
+    </figure>
+    <p>
+       Pellentesque imperdiet diam turpis, ut facilisis tellus dignissim non. Donec gravida odio ut tortor dapibus venenatis quis sed justo. Proin turpis mi, ultrices in varius sit amet, ultricies eu diam. Ut maximus quam nulla. Nulla posuere euismod augue ut molestie. Vestibulum dictum elementum sodales. Vestibulum varius massa at nisl rutrum aliquam. Suspendisse scelerisque felis faucibus ligula porttitor interdum ac a metus. Nulla vel convallis diam. Aliquam dui ipsum, ultrices sed laoreet non, suscipit sed neque. Maecenas congue purus elit, eu elementum metus vestibulum ac.
+    </p>
+    <p>
+       Aliquam accumsan dui sed pharetra ultricies. Donec ullamcorper quis felis at egestas. Phasellus euismod nec nisl sit amet porta. Duis maximus nisl quis elit aliquet scelerisque. Aenean pulvinar leo sodales urna posuere pretium. Duis at eros metus. Fusce eu risus elit. Integer eget mi ultricies, cursus odio non, rhoncus nisl. Proin mi dolor, malesuada eu tellus eget, porta sollicitudin nisi.
+    </p>
+    <p>
+       Etiam faucibus sem eu ligula efficitur, et imperdiet massa dignissim. Vivamus et varius ante, sit amet ornare odio. Aenean fringilla dui dolor, a scelerisque lectus iaculis et. Nullam ultricies ex turpis, luctus tempor lorem posuere ac. Nulla erat ligula, egestas id tortor sed, condimentum vestibulum ipsum. Nunc tincidunt blandit diam eget mattis. Vestibulum porta in ligula ut elementum. Aenean eu lobortis lectus. Integer iaculis turpis sed feugiat blandit. Nullam tincidunt non tellus id rhoncus. Sed laoreet elementum commodo.
+    </p>
   </div>
 </template>
 
